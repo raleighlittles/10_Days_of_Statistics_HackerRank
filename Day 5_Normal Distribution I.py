49e6323ce0dd1c9399adb00ae84055f9ff6e7c0f
Adekanye Francisca <damikanye@gmail.com>
2019-10-18 13:12:12 +0100

    Fixed error in the normal distribution-1

--- Day 5_Normal Distribution I.py
+++ Day 5_Normal Distribution I.py
@@ -1,20 +1,38 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+# import math
+
+# def cdf(std, avg, x):
+#     inner = 1 + math.erf((x-avg) / (std * math.sqrt(2)))
+#     return 0.5 * inner
+
+# inputs = raw_input()
+# input_single = input()
+# input_range = raw_input()
+
+# inputs = inputs.split(' ')
+# input_range = input_range.split(' ')
+
+# ans_1 =  cdf(int(inputs[1]), int(inputs[0]), input_single)
+
+# ans_2 = cdf(int(inputs[1]), int(inputs[0]), int(input_range[1])) - cdf(int(inputs[1]), int(inputs[0]), int(input_range[0]))
+
+# print round(ans_1, 3)
+# print round(ans_2, 3)
+
+# Import library
 import math
 
-def cdf(std, avg, x):
-    inner = 1 + math.erf((x-avg) / (std * math.sqrt(2)))
-    return 0.5 * inner
+# Define functions
+def cumulative(mean, std, value):
+    return 0.5 * (1 + math.erf((value - mean) / (std * (2 ** 0.5))))
 
-inputs = raw_input()
-input_single = input()
-input_range = raw_input()
+# Set data
+initial_values = list(map(float, input().split()))
+mean = initial_values[0]
+std = initial_values[1]
+less_period = float(input())
+between_period = list(map(float, input().split()))
 
-inputs = inputs.split(' ')
-input_range = input_range.split(' ')
-
-ans_1 =  cdf(int(inputs[1]), int(inputs[0]), input_single)
-
-ans_2 = cdf(int(inputs[1]), int(inputs[0]), int(input_range[1])) - cdf(int(inputs[1]), int(inputs[0]), int(input_range[0]))
-
-print round(ans_1, 3)
-print round(ans_2, 3)
+# Gets the result and show on the screen
+print (round(cumulative(mean, std, less_period),3))
+print (round(cumulative(mean, std, between_period[1]) - cumulative(mean, std, between_period[0]), 3))
